fc72e1f531f04cf0a1e83232054e4721ff8f5e0f
dpawasi <asi@dpaw.wa.gov.au>
2020-01-31 13:32:45 +0800

    minor

--- disturbance/settings.py
+++ disturbance/settings.py
@@ -86,7 +86,7 @@ CACHES = {
         'LOCATION': os.path.join(BASE_DIR, 'disturbance', 'cache'),
     }
 }
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_ds')
 STATICFILES_DIRS.append(os.path.join(os.path.join(BASE_DIR, 'disturbance', 'static')))
 DEV_STATIC = env('DEV_STATIC',False)
 DEV_STATIC_URL = env('DEV_STATIC_URL')
