8286a5dd699278a73606d480b758fdd0c82ac93d
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2020-06-16 20:38:05 +0800

    Add internal apiary_wrapper.vue

---
+++ disturbance/frontend/disturbance/src/components/internal/approvals/approval_wrapper.vue
@@ -0,0 +1,119 @@
+<template>
+<!--div class="container" id="referralWrapper"-->
+<div class="container">
+       <!--ApplicationDashTable level='external' :url='applications_url'/>
+    <div>
+        <LicenceDashTable level='external' :url='licences_url'/>
+        <ReturnDashTable level='external' :url='returns_url'/>
+    </div-->
+    <div v-if="approvalId">
+        <div v-if="apiaryApproval">
+            <ApiaryApproval :approvalId="approvalId"/>
+        </div>
+        <div v-else>
+            <Approval :approvalId="approvalId"/>
+        </div>
+    </div>
+
+</div>
+</template>
+<script>
+
+/*import datatable from '@/utils/vue/datatable.vue'
+import ApplicationDashTable from '@common-components/applications_dashboard.vue'
+import LicenceDashTable from '@common-components/licences_dashboard.vue'
+import ReturnDashTable from '@common-components/returns_dashboard.vue'
+*/
+//import Referral from './referral.vue';
+//import ApiaryReferral from './apiary_referral.vue';
+import ApiaryApproval from './apiary_approval.vue';
+import Approval from './approval.vue';
+import Vue from 'vue';
+import {
+  api_endpoints,
+  helpers
+}
+from '@/utils/hooks'
+export default {
+    name: 'ApprovalWrapper',
+    data() {
+        let vm = this;
+        return {
+            //proposal: {},
+            approvalId: null,
+            apiaryApproval: false,
+            //apiaryApplication: false,
+            /*
+            applications_url: helpers.add_endpoint_join(api_endpoints.applications_paginated,'external_datatable_list/?format=datatables'),
+            licences_url: helpers.add_endpoint_join(api_endpoints.licences_paginated,'external_datatable_list/?format=datatables'),
+            returns_url: helpers.add_endpoint_join(api_endpoints.returns_paginated,'user_datatable_list/?format=datatables'),
+            empty_list: '/api/empty_list',
+            */
+        }
+    },
+    components:{
+        /*
+        ApplicationDashTable,
+        LicenceDashTable,
+        ReturnDashTable,
+        */
+        Approval,
+        ApiaryApproval,
+    },
+    watch: {},
+    computed: {
+        /*
+        apiaryApplication: function() {
+            let retVal = false;
+            if (this.applicationTypeName === 'Apiary') {
+                retVal = true;
+            }
+            return retVal;
+        },
+        */
+        /*
+        proposalId: function() {
+            let retVal = false;
+            if (this.proposal && this.proposal.id) {
+                retVal = true;
+            }
+            return retVal;
+        },
+        apiaryApplication: function() {
+            let returnVal = false;
+            if (this.proposal && this.proposal.application_type === 'Apiary') {
+                returnVal = true;
+            }
+            console.log(returnVal)
+            return returnVal;
+        },
+        */
+    },
+    methods: {
+    },
+    mounted: function () {
+    },
+    beforeRouteEnter: function(to, from, next) {
+          Vue.http.get(`/api/approvals/${to.params.approval_id}/approval_wrapper.json`).then(res => {
+              next(vm => {
+                  console.log(res.body)
+                  vm.approvalId = res.body.id;
+                  vm.apiaryApproval = res.body.apiary_approval;
+                  /*
+                  vm.original_proposal = helpers.copyObject(res.body);
+                  if (vm.proposal.applicant) {
+                      vm.proposal.applicant.address = vm.proposal.applicant.address != null ? vm.proposal.applicant.address : {};
+                  }
+                  // Create boolean values for each application type and add logic to the following conditional
+                  if (vm.proposal && vm.proposal.application_type === 'Apiary') {
+                      vm.apiaryApplication = true;
+                  }
+                  */
+              });
+            },
+            err => {
+              console.log(err);
+            });
+    },
+}
+</script>
