c7767586aed812c007c5f55eb3f6dc991f6918dd
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-06-26 20:38:03 +0800

    Delete debug message

--- disturbance/frontend/disturbance/src/components/internal/proposals/proposed_apiary_issuance.vue
+++ disturbance/frontend/disturbance/src/components/internal/proposals/proposed_apiary_issuance.vue
@@ -1,7 +1,6 @@
 <template lang="html">
     <div id="proposedIssuanceApproval">
         <modal transition="modal fade" @ok="ok()" @cancel="cancel()" :title="title" large>
-        AHO
             <div class="container-fluid">
                 <div class="row">
                     <form class="form-horizontal" name="approvalForm">
