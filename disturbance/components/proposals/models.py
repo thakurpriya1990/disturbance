119dca556eb9eaaed62af40fbfb78aef958bd61f
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-06-29 21:16:42 +0800

    Add a new field named workflow_selected_status to store the selection status during approval process

--- disturbance/components/proposals/models.py
+++ disturbance/components/proposals/models.py
@@ -2623,6 +2623,7 @@ class ApiarySite(models.Model):
     region = models.ForeignKey(Region, null=True, blank=True)
     district = models.ForeignKey(District, null=True, blank=True)
     status = models.CharField(max_length=40, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    workflow_selected_status = models.BooleanField(default=False)  # This field is used only during approval process to select/deselect the site to be approved
     wkb_geometry = PointField(srid=4326, blank=True, null=True)
     objects = GeoManager()
 
