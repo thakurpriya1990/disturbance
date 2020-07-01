c3329636561fa19e541a3c7a6e9a956af05986e9
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-07-01 16:42:26 +0800

    Add fields, not completed

--- disturbance/frontend/disturbance/src/components/common/apiary/section_annual_rental_fee.vue
+++ disturbance/frontend/disturbance/src/components/common/apiary/section_annual_rental_fee.vue
@@ -1,7 +1,33 @@
 <template lang="html">
     <div>
 
-        Body annual rental fee
+        <div class="form-group row">
+            <label class="col-sm-5">Do not charge annual rental fee until</label>
+            <div class="col-sm-6">
+                <div class="input-group date" ref="periodFromDatePicker">
+                    <input type="text" class="form-control" placeholder="DD/MM/YYYY" id="no_charge_until" :readonly="is_readonly"/>
+                    <span class="input-group-addon">
+                        <span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-sm-5">Calendar year</label>
+            <div class="col-sm-6">
+                <select class="form-control">
+                    <option>AHO</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-sm-5">Invoice</label>
+            <div class="col-sm-6">
+                invoices here
+            </div>
+        </div>
 
     </div>
 </template>
