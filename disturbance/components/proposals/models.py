89e1ba5e8a7e276c0eefe6375092d7ccc5dc0a8b
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-08-27 09:51:01 +0800

    Add a new geometry field to store the geometry moved by the assessor and/or approver during processing

--- disturbance/components/proposals/models.py
+++ disturbance/components/proposals/models.py
@@ -3243,8 +3243,9 @@ class ApiarySite(models.Model):
     district = models.ForeignKey(District, null=True, blank=True)
     status = models.CharField(max_length=40, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
     workflow_selected_status = models.BooleanField(default=False)  # This field is used only during approval process to select/deselect the site to be approved
-    wkb_geometry = PointField(srid=4326, blank=True, null=True)
-    wkb_geometry_applied = PointField(srid=4326, blank=True, null=True)
+    wkb_geometry = PointField(srid=4326, blank=True, null=True)  # store approved geometry
+    wkb_geometry_applied = PointField(srid=4326, blank=True, null=True)  # store original geometry
+    wkb_geometry_pending = PointField(srid=4326, blank=True, null=True)  # store the geometry moved by the assessor and/or approver during processing
     objects = GeoManager()
 
     def __str__(self):
