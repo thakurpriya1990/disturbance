5643297503b07394314fe0c6bf53d8c9500d4e24
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-09-15 11:48:20 +0800

    Forgot to add this file to the previous commit

--- disturbance/components/proposals/models.py
+++ disturbance/components/proposals/models.py
@@ -12,7 +12,7 @@ from django.db import models,transaction
 from django.contrib.gis.db import models as gis_models
 from django.db.models import Q
 from django.dispatch import receiver
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.utils.encoding import python_2_unicode_compatible
 from django.core.exceptions import ValidationError
 from django.contrib.postgres.fields.jsonb import JSONField
