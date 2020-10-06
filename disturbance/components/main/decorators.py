bb12b1b5e4c23b403f3230341b09d4219e78af1a
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-10-06 09:57:36 +0800

    Add a function to measure execution time of a function

--- disturbance/components/main/decorators.py
+++ disturbance/components/main/decorators.py
@@ -1,3 +1,4 @@
+import time
 import traceback
 from django.core.exceptions import ValidationError
 from rest_framework import serializers
@@ -20,3 +21,18 @@ def basic_exception_handler(func):
             print(traceback.print_exc())
             raise serializers.ValidationError(str(e))
     return wrapper
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print '%r  %2.2f ms' % \
+                  (method.__name__, (te - ts) * 1000)
+        return result
+    return timed
