3b6f5550d507cc427af2ae9b2b9b362c8797c15a
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-10-20 15:03:38 +0800

    Fix a bug where Site Transfer applications show Unavailable label in action column for sites to be transferred. That label should not show for internal users (for external users we show a Make available and Make unavailable action when opening a licence, but only for external users); Make the checkbox column of the site transfer application disabled for the internal user

--- disturbance/frontend/disturbance/src/components/form_apiary_site_transfer.vue
+++ disturbance/frontend/disturbance/src/components/form_apiary_site_transfer.vue
@@ -96,6 +96,9 @@
                     :is_external="is_external"
                     :key="component_site_selection_key"
                     :show_col_checkbox="showColCheckbox"
+                    :enable_col_checkbox="is_external"
+                    :show_action_available_unavailable="is_external"
+
                     ref="component_site_selection"
                     @apiary_sites_updated="apiarySitesUpdated"
                   />
@@ -120,7 +123,7 @@
                 </div>
             </FormSection>
 
-            <ApiaryChecklist 
+            <ApiaryChecklist
                 :checklist="applicantChecklistAnswers"
                 section_title="Applicant Checklist"
                 :readonly="readonly"
