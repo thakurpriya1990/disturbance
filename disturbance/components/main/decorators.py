088be862b1548687c3e1da544ad331e91c7eb1d4
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-11-26 15:48:19 +0800

    First commit for the python3.6

--- disturbance/components/main/decorators.py
+++ disturbance/components/main/decorators.py
@@ -32,7 +32,6 @@ def timeit(method):
             name = kw.get('log_name', method.__name__.upper())
             kw['log_time'][name] = int((te - ts) * 1000)
         else:
-            print '%r  %2.2f ms' % \
-                  (method.__name__, (te - ts) * 1000)
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
         return result
     return timed
