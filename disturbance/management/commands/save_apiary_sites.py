76323130b54e8a61ccbfddf5fd66a13813c201bf
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-12-24 11:41:30 +0800

    Keep latest x number of files

--- disturbance/management/commands/save_apiary_sites.py
+++ disturbance/management/commands/save_apiary_sites.py
@@ -1,7 +1,10 @@
+import datetime
 import json
 import os
 
+import pytz
 from django.core.management.base import BaseCommand
+from ledger.settings_base import TIME_ZONE
 
 from disturbance.components.approvals.serializers_apiary import ApiarySiteOnApprovalGeometryExportSerializer
 from disturbance.components.main.utils import get_qs_vacant_site, get_qs_proposal, get_qs_approval
@@ -43,6 +46,12 @@ class Command(BaseCommand):
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
-        file_path = os.path.join(save_dir, 'test.json')  # TODO: we jsut want to keep latest 3 files or so.
+        datetime_local = datetime.datetime.now(pytz.timezone(TIME_ZONE)).strftime('%Y%m%d-%H%M%S')
+        file_path = os.path.join(save_dir, '{}-apiary-sites.json'.format(datetime_local))
         with open(file_path, 'w') as fp:
             json.dump(serializer_approval.data, fp)
+
+        files = os.listdir(save_dir)
+        files = sorted(files, reverse=True)  # sort by descending order
+        for file in files[3:]:
+            os.remove(os.path.join(save_dir, file))
