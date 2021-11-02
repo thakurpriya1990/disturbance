a3e9c74595a12f9dc7620d2bf832af5b7633ec74
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-11-02 14:06:37 +0800

    Remove unused field

--- disturbance/components/main/admin.py
+++ disturbance/components/main/admin.py
@@ -5,4 +5,4 @@ from disturbance.components.main.models import MapLayer
 
 @admin.register(MapLayer)
 class AmendmentReasonAdmin(admin.ModelAdmin):
-    list_display = ['display_name', 'layer_name', 'availability', 'option_for_internal', 'option_for_external',]
+    list_display = ['display_name', 'layer_name', 'option_for_internal', 'option_for_external',]
--- disturbance/components/main/models.py
+++ disturbance/components/main/models.py
@@ -14,7 +14,6 @@ from django.contrib.postgres.fields.jsonb import JSONField
 class MapLayer(models.Model):
     display_name = models.CharField(max_length=100, blank=True, null=True)
     layer_name = models.CharField(max_length=200, blank=True, null=True)  # layer name defined in geoserver (kmi.dpaw.wa.gov.au)
-    availability = models.BooleanField(default=True)  # False to hide from the frontend options
     option_for_internal = models.BooleanField(default=True)
     option_for_external = models.BooleanField(default=True)
 
