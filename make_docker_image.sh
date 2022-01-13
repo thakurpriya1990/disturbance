d91969eed2b50480448a74e579d6990370a647ff
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2022-01-13 12:42:43 +0800

    updated requirements.txt

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ## sole parameter is an integer indicating incremental daily version
+## git branch --set-upstream-to=origin/das_dev das_dev	
 
 if [ $# -lt 2 ]; then
     echo "ERROR: Must specify <github branch> and <integer indicating incremental daily version> e.g."
@@ -13,8 +14,8 @@ fi
 
 GIT_BRANCH=$1
 BUILD_TAG=dbcawa/disturbance:v$(date +%Y.%m.%d).$2
-#git checkout $GIT_BRANCH &&
-#git pull &&
+git checkout $GIT_BRANCH &&
+git pull &&
 cd disturbance/frontend/disturbance/ &&
 npm run build &&
 cd ../../../ &&
