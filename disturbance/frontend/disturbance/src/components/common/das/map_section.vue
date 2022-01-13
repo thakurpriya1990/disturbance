53565893d8c312299a14c58263d4aa4d620fc5ba
preranaandure <prerana.andure@dbca.wa.gov.au>
2022-01-13 10:35:38 +0800

    Map section start

---
+++ disturbance/frontend/disturbance/src/components/common/das/map_section.vue
@@ -0,0 +1,128 @@
+<template lang="html">
+    <div class="">
+        <div class="col-md-3">
+        </div>
+        <div :class="das_sections_classname">
+            <FormSection :formCollapse="false" label="Map" Index="1">
+                <div>ABC
+                </div>
+                <div>                    
+                </div>
+            </FormSection>  
+        </div>
+    </div>
+</template>
+
+<script>
+    
+    import FileField from '@/components/forms/filefield_immediate.vue'
+    import FormSection from "@/components/forms/section_toggle.vue"
+    import uuid from 'uuid'
+    import { api_endpoints, helpers }from '@/utils/hooks'
+    export default {
+        name: 'ApiaryForm',
+        props:{
+            proposal:{
+                type: Object,
+                required:true
+            },
+            canEditActivities:{
+              type: Boolean,
+              default: true
+            },
+            is_external:{
+              type: Boolean,
+              default: false
+            },
+            is_internal:{
+              type: Boolean,
+              default: false
+            },
+            is_referral:{
+              type: Boolean,
+              default: false
+            },
+            hasReferralMode:{
+                type:Boolean,
+                default: false
+            },
+            hasAssessorMode:{
+                type:Boolean,
+                default: false
+            },
+            referral:{
+                type: Object,
+                required:false
+            },
+            proposal_parks:{
+                type:Object,
+                default:null
+            },
+        },
+        data:function () {
+            let vm = this;
+            return{
+                values:null,
+                pBody: 'pBody'+vm._uid,
+                
+            }
+        },
+        components: {
+            FileField,
+            FormSection,
+        },
+        computed:{
+            das_sections_classname: function() {
+                // For external page, we need 'col-md-9' classname
+                // but not for the internal.
+                // This is a hacky way, though...
+                if(this.is_internal){
+                    return ''
+                } else {
+                    return 'col-md-9'
+                }
+            },
+        },
+        methods:{
+            
+            
+        },
+        created: function() {
+        },
+        mounted: function() {
+            let vm = this;
+            
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    .section{
+        text-transform: capitalize;
+    }
+    .list-group{
+        margin-bottom: 0;
+    }
+    .fixed-top{
+        position: fixed;
+        top:56px;
+    }
+    .insurance-items {
+        padding-inline-start: 1em;
+    }
+    .my-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .grow1 {
+        flex-grow: 1;
+    }
+    .grow2 {
+        flex-grow: 2;
+    }
+    .input-file-wrapper {
+        margin: 1.5em 0 0 0;
+    }
+</style>
+
