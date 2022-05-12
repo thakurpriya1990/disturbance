bbc5ecd7d9bdd47cc6a978f001a2faa3c5206805
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-05-12 11:20:51 +0800

    Added a View to compare a specific field between any two model versions

--- disturbance/components/history/api.py
+++ disturbance/components/history/api.py
@@ -108,3 +108,42 @@ class GetCompareVersionsView(InternalAuthorizationView):
         logger.info('\n\nformatted_differences = %s \n\n', formatted_differences)   
 
         return Response(literal_eval(formatted_differences))
+
+class GetCompareFieldVersionsView(InternalAuthorizationView):
+    """ Returns the difference for a specific field
+     between two specific versions of any model object
+
+        api/history/compare/app_label/model_name/pk/newer_version/older_version/compare_field/
+
+        Example:
+
+        api/history/compare/disturbance/Proposal/1933/0/2/data/
+
+    """
+    def get(self, request, app_label, model_name, pk, newer_version, older_version, compare_field):
+        """ Returns the difference between two specific versions of any model object """
+        super().get(self)
+
+        logger.info('app_label = %s', app_label)
+        logger.info('model_name = %s', model_name)
+        logger.info('pk = %s', pk)
+
+        model = apps.get_model(app_label=app_label, model_name=model_name)
+
+        logger.info('model = %s', model)
+
+        instance = model.objects.get(pk=int(pk))
+
+        newer_version = Version.objects.get_for_object(instance)[int(newer_version)]
+        older_version = Version.objects.get_for_object(instance)[int(older_version)]
+
+        differences = DeepDiff(newer_version.field_dict[compare_field], older_version.field_dict[compare_field], ignore_order=True)
+
+        default_mapping = {datetime.datetime: lambda d: str(d)}
+
+        formatted_differences = json.dumps(json.loads(differences.to_json(
+            default_mapping=default_mapping)), indent=4, sort_keys=True)
+
+        logger.info('\n\nformatted_differences = %s \n\n', formatted_differences)   
+
+        return Response(literal_eval(formatted_differences))
