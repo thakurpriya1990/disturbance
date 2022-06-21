8f7f0a9d3278726096ad3ba369f4aaa23df0fd3a
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2022-06-21 14:47:51 +0800

    Fix minor bugs

--- disturbance/components/approvals/serializers.py
+++ disturbance/components/approvals/serializers.py
@@ -408,7 +408,7 @@ class ApprovalSerializer(serializers.ModelSerializer):
 
     def get_apiary_sites(self, approval):
         with_apiary_sites = True
-        if hasattr(self.context, 'request'):
+        if 'request' in self.context:
             request = self.context['request']
             with_apiary_sites = request.GET.get('with_apiary_sites', True)
             if with_apiary_sites in ['false', 'False', 'F', 'f', False]:
