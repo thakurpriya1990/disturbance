07b441dde1f346ea0260b2503864265de64b731e
preranaandure <preranapj07@gmail.com>
2023-05-16 11:33:48 +0800

    Refresh component (WIP)

---
+++ disturbance/frontend/disturbance/src/components/forms/refresh.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+        <span>
+            <template v-if="!assessorMode">
+                <template>
+                    <a href="" @click.prevent="refresh">Refresh&nbsp;</i></a>
+                </template>
+            </template>
+        </span>     
+</template>
+
+ 
+
+
+
+<script>
+export default {
+    props:["parent_name","assessorMode","layer_data"],
+
+components: {  },
+data: function() {
+  return{
+    showingHelpText: false,
+    pBody: 'pBody'
+    }
+  },
+
+ methods:{
+         refresh: function(){
+            let vm=this;
+            console.log("inside refresh,", vm.parent_name)
+            var ele=$('[name='+vm.parent_name+']')[0]
+           ele.value='123';
+
+        },
+   }
+}
+</script>
+
+<style lang="css">
+</style>
+
+
