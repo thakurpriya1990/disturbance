732026983cc1793d27fecf30820cc2573244dc00
preranaandure <preranapj07@gmail.com>
2023-06-27 11:54:31 +0800

    Add action log for refresh and Prefill functions

--- disturbance/components/proposals/api.py
+++ disturbance/components/proposals/api.py
@@ -80,6 +80,7 @@ from disturbance.components.proposals.models import (
     MasterlistQuestion,
     CddpQuestionGroup,
     SpatialQueryQuestion,
+    ProposalUserAction,
 )
 from disturbance.components.proposals.serializers import (
     SendReferralSerializer,
@@ -1697,6 +1698,7 @@ class ProposalViewSet(viewsets.ModelViewSet):
                 if 'sqs_timestamp' in layer_data:
                     answer_response['sqs_timestamp']=layer_data['sqs_timestamp']
             proposal.save()
+            proposal.log_user_action(ProposalUserAction.ACTION_REFRESH_PROPOSAL.format(proposal.lodgement_number), request)
         #refresh the add_info_assessor
         # if 'add_info_assessor' in sqs_resp:
         #     proposal.add_info_assessor[schema_name]= sqs_resp['add_info_assessor']
@@ -2369,7 +2371,8 @@ class ProposalViewSet(viewsets.ModelViewSet):
                     instance.save()
                 else:
                     raise serializers.ValidationError(str('Please upload a valid shapefile'))                   
-            instance.save()
+            instance.save(version_comment='Prefill Proposal')
+            instance.log_user_action(ProposalUserAction.ACTION_PREFILL_PROPOSAL.format(instance.lodgement_number), request)
             serializer = self.get_serializer(instance)
             return Response(serializer.data)
             #return redirect(reverse('external'))
