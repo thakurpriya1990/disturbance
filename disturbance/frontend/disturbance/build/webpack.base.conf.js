ae781b21b7111673baafdf7a0a0d149580018259
preranaandure <preranapj07@gmail.com>
2023-09-29 10:01:16 +0800

    Rich text editor for Help text and Help text assessor

--- disturbance/frontend/disturbance/build/webpack.base.conf.js
+++ disturbance/frontend/disturbance/build/webpack.base.conf.js
@@ -58,7 +58,8 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/ckeditor4'),
+        resolve('node_modules/ckeditor4-vue') ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
