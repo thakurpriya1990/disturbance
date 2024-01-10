1ea70f0b4ea0ac3fbc3af4278dde6bdfc88b77c6
preranaandure <preranapj07@gmail.com>
2024-01-10 10:26:22 +0800

    Make Proposal issue details non mandatory

--- disturbance/components/proposals/serializers.py
+++ disturbance/components/proposals/serializers.py
@@ -704,7 +704,7 @@ class ProposalStandardRequirementSerializer(serializers.ModelSerializer):
 class ProposedApprovalSerializer(serializers.Serializer):
     expiry_date = serializers.DateField(input_formats=['%d/%m/%Y'], required=False)
     start_date = serializers.DateField(input_formats=['%d/%m/%Y'], required=False)
-    details = serializers.CharField()
+    details = serializers.CharField(required=False)
     cc_email = serializers.CharField(required=False,allow_null=True, allow_blank=True)
     confirmation = serializers.BooleanField(required=False,default=False)
 
