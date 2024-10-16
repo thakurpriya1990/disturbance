41eb3dafd2b536889abef5d35d9ef83d4c17e78e
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2024-10-16 14:17:18 +0800

    python-cron and updated healthcheck content in Dockerfile

--- startup.sh
+++ startup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Start the first process
-env > /etc/.cronenv
-sed -i 's/\"/\\"/g' /etc/.cronenv
+#env > /etc/.cronenv
+#sed -i 's/\"/\\"/g' /etc/.cronenv
 cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 if [ $ENABLE_CRON == "True" ];
-then
-echo "Starting Cron"
-service cron start &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start cron: $status"
-  exit $status
+    then
+    echo "Starting Python Cron"
+    python /bin/scheduler.py /app/python-cron /app/logs/python-cron.log &
+    status=$?
+    if [ $status -ne 0 ]; then
+        echo "Failed to start cron: $status"
+        exit $status
+    fi
 fi
 
-fi
 
 if [ $ENABLE_WEB == "True" ];
     then
